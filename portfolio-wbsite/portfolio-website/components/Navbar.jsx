9030a6e2a3f84ff9728fc93c8f67a1e97d53775f
Murat Koyun <muratk1845@gmail.com>
2023-12-05 10:05:32 +0100

    navlinks

--- portfolio-wbsite/portfolio-website/components/Navbar.jsx
+++ portfolio-wbsite/portfolio-website/components/Navbar.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
 import Link from 'next/link'
+import NavLink from './NavLink'
+
+
+const navLinks =[
+  {
+    title: "About",
+    path: "#about"
+  },
+  {
+    title: "Projects",
+    path: "#projects"
+  },
+  {
+    title: "Contact",
+    path: "#contact"
+  
+  }
+]
 
 const Navbar = () => {
   return (
     <nav>
         <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
             <Link href={"/"} className='text-3xl text-white font-semibold'>Murat.dev</Link>
-            <div className='menu'></div>
+            <div id='navbar' className='menu block md:w-auto'>
+              <ul>
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
+              </ul>
+            </div>
         </div>
     </nav>
   )
